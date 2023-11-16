96ebbc5b2c49b2c94c4f4e41aec08e8ad0912cc6
gundeep <gundeep.upenn@gmail.com>
2023-11-16 21:17:11 +0100

    move things around discussion with nafeh

--- AleoWorkshop/HomeView.swift
+++ AleoWorkshop/HomeView.swift
@@ -21,63 +21,50 @@ struct HomeView: View {
     @State var selectedRequest: ShareRequest?
     @State var text = ""
     @State private var isTooltipVisible = false
-
+    
     
     var body: some View {
-        
         NavigationStack {
-            List {
-                ForEach(shareRequests) { request in
-                    Button {
-                        selectedRequest = request
-                    } label: {
-                        Text("Request from \(request.source) at \(request.date, format: Date.FormatStyle(date: .numeric, time: .standard))")
+            ZStack(alignment: .center) {
+                List {
+                    ForEach(shareRequests) { request in
+                        Button {
+                            selectedRequest = request
+                        } label: {
+                            Text("Request from \(request.source) at \(request.date, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        }
+                    }
+                    .onDelete(perform: deleteRequest)
+                    .sheet(item: $selectedRequest) { request in
+                        RequestView(shareRequest: request)
                     }
                 }
-                .onDelete(perform: deleteRequest)
-                .sheet(item: $selectedRequest) { request in
-                    RequestView(shareRequest: request)
-                }
+                TextField("Enter Wallet Address Here", text: $text)
+                    .padding()
+                    .border(Color.gray, width: 0.5)
+                    .gesture(
+                        LongPressGesture(minimumDuration: 0.5)
+                            .onChanged { _ in isTooltipVisible = true }
+                            .onEnded { _ in isTooltipVisible = false }
+                    )
+                    .overlay(alignment: .topTrailing) {
+                        Group {
+                            if isTooltipVisible {
+                                Text("Your wallet address will never leave the device")
+                                    .padding()
+                                    .background(Color.white)
+                                    .foregroundStyle(Color.black)
+                                    .shadow(radius: 5)
+                                    .offset(y: -50)
+                            }
+                        }
+                    }
             }
             .navigationTitle("Welcome \(name)!")
         }
-        
-        VStack {
-            TextField("Enter Wallet Address Here", text: $text)
-                .padding()
-                .border(Color.gray, width: 0.5)
-                .gesture(LongPressGesture(minimumDuration: 0.5)
-                    .onChanged { _ in isTooltipVisible = true }
-                    .onEnded { _ in isTooltipVisible = false })
-                .overlay(
-                    Group {
-                        if isTooltipVisible {
-                            Text("Your wallet address will never leave the device")
-                                .padding()
-                                .background(Color.white)
-                                .foregroundColor(Color.black)
-                                .shadow(radius: 5)
-                                .offset(y: -50)
-                        }
-                    }, alignment: .topTrailing
-                )
-        }
-        // write code input field to view
-        //
-         //let textField = UITextField(frame: CGRect(x: 20, y: 100, width: 300, height: 40))
-         //textField.borderStyle = UITextField.BorderStyle.roundedRect
-         //textField.placeholder = "Enter text here"
-        //
-        //
-
-
-        //self.view.addSubview(textField)
     }
     
-
-
     func deleteRequest(_ indexSet: IndexSet?) {
-        
     }
     
 }
